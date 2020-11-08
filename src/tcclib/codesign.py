eed61a82cb932ed24dce02d3b0b224c448911f9d
carlashley <git@macapakr.com>
2020-11-08 21:31:56 +1000

    Deatched signature for scripts - may be removed

--- src/tcclib/codesign.py
+++ src/tcclib/codesign.py
@@ -1,7 +1,11 @@
 """Codesign."""
 
-import subprocess
 import re
+import subprocess
+
+from os import remove
+from pathlib import Path, PurePath
+from tempfile import gettempdir
 
 
 def _xxd(blob):
@@ -18,62 +22,6 @@ def _xxd(blob):
         result = _r
     elif _p.returncode != 0 and _e:
         result = _e
-
-    return result
-
-
-def requirements(path):
-    """Codesign."""
-    result = dict()
-
-    # Lines with useful output start with these strings
-    _dsgn_prefix = 'designated => '
-    _idnt_prefix = 'Identifier='
-
-    # Bundle ID regex test string
-    _bnid_regex = re.compile(r'^\w+\.')
-
-    _cmd = ['/usr/bin/codesign', '-v', '-dr', '-', path]
-    _p = subprocess.Popen(_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    _r, _e = _p.communicate()
-
-    if _r and isinstance(_r, bytes):
-        _r = _r.decode('utf-8')
-
-    if _e and isinstance(_e, bytes):
-        _e = _e.decode('utf-8')
-
-    if _p.returncode == 0:
-        # Extract the code signature from the result. Handle circumstances
-        # where the output may not explicitly start with 'designated => '
-        # and where the code signature is split across multiple lines.
-        for _line in _r.splitlines():
-            if _dsgn_prefix in _line:
-                _res = _line.partition(_dsgn_prefix)
-                _res = _res[_res.index(_dsgn_prefix) + 1:][0]
-                result['csreq'] = _res
-
-        for _line in _e.splitlines():
-            if _line.startswith(_idnt_prefix):
-                if _idnt_prefix in _line:
-                    result['client'] = _line.replace(_idnt_prefix, '')
-                    result['client_type'] = 'bundleID'
-
-                    # Test to make sure that the bundle ID matches
-                    # expected style of 'org.example.foo'. This is
-                    # not a terribly strict test, but it is aimed to
-                    # avoid scenarios like the 'python3' binary
-                    # deep in the framework has an identifier value
-                    # of 'python3'.
-                    if not re.match(_bnid_regex, result['client']):
-                        result['client'] = path
-                        result['client_type'] = 'path'
-    elif _p.returncode == 1 and 'not signed' in _e:
-        result['csreq'] = None
-        result['client'] = None
-        result['client_type'] = None
-
-    result['is_signed'] = result.get('csreq', None) is not None
 
     return result
 
@@ -98,5 +46,92 @@ def csreq(blob):
         result = _r
     elif _p.returncode != 0 and _e:
         result = _e
+
+    return result
+
+
+def detached_signature(path):
+    """Codesign using a detached signature."""
+    result = None
+
+    _tmp = gettempdir()
+    _path_sig = '{}.sig'.format(PurePath(path).name)
+    _sig_file = Path(_tmp) / _path_sig
+
+    _cmd = ['/usr/bin/codesign', '--detached', str(_sig_file), '-s', '-', path]
+    _p = subprocess.Popen(_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    _r, _e = _p.communicate()
+
+    if _r and isinstance(_r, bytes):
+        _r = _r.decode('utf-8')
+
+    if _e and isinstance(_e, bytes):
+        _e = _e.decode('utf-8')
+
+    if _p.returncode == 0:
+        result = requirements(path, detached_sig=_sig_file)
+
+        if result:
+            remove(str(_sig_file))
+
+    return result
+
+
+def requirements(path, detached_sig=None):
+    """Codesign."""
+    result = dict()
+
+    # Lines with useful output start with these strings
+    _dsgn_prefix = 'designated => '
+    _idnt_prefix = 'Identifier='
+
+    # Bundle ID regex test string
+    _bnid_regex = re.compile(r'^\w+\.')
+
+    if not detached_sig:
+        _cmd = ['/usr/bin/codesign', '-v', '-dr', '-', path]
+    elif detached_sig:
+        _cmd = ['/usr/bin/codesign', '-vvv', '-d', '-r', '-', '--detached', detached_sig, path]
+
+    _p = subprocess.Popen(_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    _r, _e = _p.communicate()
+
+    if _r and isinstance(_r, bytes):
+        _r = _r.decode('utf-8')
+
+    if _e and isinstance(_e, bytes):
+        _e = _e.decode('utf-8')
+
+    if _p.returncode == 0:
+        # Extract the code signature from the result. Handle circumstances
+        # where the output may not explicitly start with 'designated => '
+        # and where the code signature is split across multiple lines.
+        for _line in _r.splitlines():
+            if _dsgn_prefix in _line:
+                _res = _line.partition(_dsgn_prefix)
+                _res = _res[_res.index(_dsgn_prefix) + 1:][0]
+                result['csreq'] = _res
+
+        for _line in _e.splitlines():
+            if _line.startswith(_idnt_prefix):
+                if _idnt_prefix in _line:
+                    result['identifier'] = _line.replace(_idnt_prefix, '')
+                    result['identifier_type'] = 'bundleID'
+
+                    # Test to make sure that the bundle ID matches
+                    # expected style of 'org.example.foo'. This is
+                    # not a terribly strict test, but it is aimed to
+                    # avoid scenarios like the 'python3' binary
+                    # deep in the framework has an identifier value
+                    # of 'python3'.
+                    if not re.match(_bnid_regex, result['identifier']):
+                        result['identifier'] = path
+                        result['identifier_type'] = 'path'
+    elif _p.returncode == 1 and 'not signed' in _e:
+        result['csreq'] = None
+        result['identifier'] = None
+        result['identifier_type'] = None
+
+    result['is_signed'] = result.get('csreq', None) is not None
 
     return result
